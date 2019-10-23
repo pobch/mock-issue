ba6cab7b8fefe1d5bda9ab65fff0a3062178b089
Pob <590650@gmail.com>
2019-10-23 10:29:44 +0700

    add solution to the codebase

--- src/App.test.js
+++ src/App.test.js
@@ -4,20 +4,39 @@ import { render } from '@testing-library/react'
 import App from './App'
 import { myModule as mockMyModule } from './myModule'
 
+// -------------- MOCK
 jest.mock('./myModule', () => {
   return {
     myModule: jest.fn()
   }
 })
 
+afterEach(() => {
+  // Reset to the jest.mock() version
+  jest.resetAllMocks()
+})
+
+// -------------- TEST CASES
 test('1 - with mocked module', () => {
-  // This test works fine since I use the mocked module
+  // This test works fine since I use the mocked module from jest.mock()
   render(<App />)
   expect(mockMyModule).toHaveBeenCalled()
 })
 
 test('2 - with original module', () => {
   // Instead of the mocked module, I need to use original module in this test
+
+  // Restore to the original module
+  mockMyModule.mockImplementationOnce(require.requireActual('./myModule').myModule)
+
   const { getByText } = render(<App />)
-  expect(getByText(/original text/i)).toBeInTheDocument() // Not work
+  expect(getByText(/original text/i)).toBeInTheDocument()
+})
+
+test('3 - with mocked module again', () => {
+  // This test needs the mocked module from jest.mock() again
+  // Test whether `mockMyModule` was resetted to the initial jest.mock() version
+  const { queryByText } = render(<App />)
+  expect(mockMyModule).toHaveBeenCalledTimes(1)
+  expect(queryByText(/original text/i)).toBeNull()
 })
